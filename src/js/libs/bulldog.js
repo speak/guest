6bf1d4397c4a147654df095766e5f7088ec06057
Luke Roberts <luke@sqwiggle.com>
2015-10-22 17:07:51 -0700

    port libs from old guest repo

--- src/js/libs/bulldog.js
+++ src/js/libs/bulldog.js
@@ -1,10 +1,23 @@
 var BulldogActions = require('../actions/bulldog-actions');
+var utils = require('./utilities');
 var $ = require('jquery-browserify');
 var _ = require('underscore');
 var Config = require('config');
 
 var Bulldog = {
-
+  
+  createUser: function(data, opts) {
+    if(!opts) {
+      opts = {};
+    }
+    Bulldog.post({
+      endpoint: '/users',
+      data: data
+    })
+    .done(opts.success || BulldogActions.signedIn)
+    .fail(opts.error || this.dispatchError);
+  },
+  
   createSessionFromEmailPassword: function(data) {
     Bulldog.post({
       endpoint: '/sessions',
@@ -13,7 +26,97 @@ var Bulldog = {
     .done(BulldogActions.signedIn)
     .fail(this.dispatchError);
   },
+  
+  createSessionFromToken: function(token) {
+    var self = this;
+    
+    Bulldog.post({
+      endpoint: '/tickets',
+      data: {
+        token: token
+      }
+    })
+    .done(function(data){
+      clearTimeout(self.timeoutRetry);
+      BulldogActions.signedIn(data);
+    })
+    .fail(function(xhr) {
+      // server couldn't be reached, retry
+      if (xhr.status == 0 || xhr.status == 500) {
+        self.timeoutRetry = setTimeout(function(){
+          self.createSessionFromToken(token);
+        }, 3*1000);
+      } else {
+        self.dispatchError(xhr);
+      }
+    });
+  },
 
+  updatePassword: function(password, password_confirmation, token, success, error) {
+    var self = this;
+    Bulldog.put({
+      endpoint: '/password',
+      data: {
+        password: password,
+        password_confirmation: password_confirmation,
+        token: token
+      }
+    })
+    .done(function(data){
+      if(success) {
+        success();
+      } else {
+        BulldogActions.passwordUpdated(data);
+      }
+    })
+    .fail(function(xhr) {
+      if(error) {
+        error();
+      } else {
+        BulldogActions.passwordUpdateFailed(JSON.parse(xhr.responseText));
+      }
+    })
+  },
+
+  createUploadAuth: function(token, key, type, success, error) {
+    Bulldog.post({
+      endpoint: '/upload/auth',
+      data: {
+        token: token,
+        key: key,
+        bucket: Config.storage.avatars,
+        content_type: type,
+        acl: 'public-read'
+      }
+    })
+    .done(function(data){
+      if(typeof success == "function") {
+        success(data);
+      }
+    })
+    .fail(function(xhr) {
+      if(typeof error == "function") {
+        error(xhr);
+      }
+    });
+  },
+
+  createPasswordReset: function(email){
+    var self = this;
+    Bulldog.post({
+      endpoint: '/password/reset',
+      data: {
+        email: email
+      }
+    })
+    .done(function(data){
+      BulldogActions.passwordResetRequested();
+    })
+    .fail(function(xhr) {
+      self.dispatchError(xhr);
+    });
+  },
+  
   dispatchError: function(xhr) {
     var response;
     
@@ -31,6 +134,11 @@ var Bulldog = {
     data.type = 'GET';
     return this.request(data);
   },
+
+  put: function(data) {
+    data.type = 'PUT';
+    return this.request(data);
+  },
   
   post: function(data) {
     data.type = 'POST';
@@ -42,15 +150,25 @@ var Bulldog = {
     options.data = options.data || {};
     
     if (options.endpoint) options.url += options.endpoint;
-
-    // TODO: generate
-    options.data.device_id = 123;
+    
+    // add the device_id to every auth request
+    options.data.device_id = this.getDeviceId();
     
     // we're only dealing with JSON.
     options.crossDomain = true;
     options.dataType = 'json';
     
     return $.ajax(options);
+  },
+  
+  getDeviceId: function() {
+    var device_id;
+    device_id = localStorage.getItem('device_id');
+    if (!device_id) {
+      device_id = utils.guid();
+      localStorage.setItem('device_id', device_id);
+    }
+    return device_id;
   }
 };
 
