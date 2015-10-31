9e6c6f4cae107e3a6a30e22bec455d875014fe62
Luke Roberts <luke@sqwiggle.com>
2015-10-30 20:23:51 -0700

    load existing users from the channel

--- src/js/stores/users-store.js
+++ src/js/stores/users-store.js
@@ -4,7 +4,16 @@ var _ = require('underscore');
 
 var UsersStore = Flux.createStore({
   actions: {
+    'channel.found': 'channelUpdated',
+    'channel.created': 'channelUpdated',
     'channel.joined': 'channelJoined'
+  },
+
+  channelUpdated: function(data) {
+    data.users.forEach(function(u){
+      this.state[u.id] = u;
+    }.bind(this));
+    this.emit('change');
   },
 
   channelJoined: function(data){
