0d2b539b9d48d7450f1b792f7209cb48f3fd70aa
Tom Moor <tom.moor@gmail.com>
2015-11-02 21:58:20 -0800

    Added: Copy meeting url to clipboard

--- src/js/components/channel-info.jsx
+++ src/js/components/channel-info.jsx
@@ -1,13 +1,24 @@
 var React = require('react');
 var Config = require('config');
+var Clipboard = require('clipboard');
 
 var ChannelInfo = React.createClass({
+  
+  componentDidMount: function() {
+    this.setState({clipboard: new Clipboard(this.refs.copyable) });
+  },
+  
+  componentWillUnmount: function() {
+    this.state.clipboard.destroy();
+  },
+  
   render: function() {
-    var host = Config.hosts.app.replace(/^https?:\/\//gi, "");
+    var url = Config.hosts.app + "/" + this.props.path;
+    var display = url.replace(/^https?:\/\//gi, "");
     
     return <div id="channel-info">
       <h2>Your Meeting URL</h2>
-      <h3>{host + "/" + this.props.path}</h3>
+      <h3 data-clipboard-text={url} ref="copyable">{display}</h3>
     </div>;
   }
 });
