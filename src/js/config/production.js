2623bbf603982669e9e9dda23eb7a7f864ff9c02
Tom Moor <tom.moor@gmail.com>
2015-11-04 15:28:15 -0800

    Added: Staging config

--- src/js/config/production.js
+++ src/js/config/production.js
@@ -12,6 +12,7 @@ module.exports = {
     photos: 'speak-production-photos'
   },
   tokens: {
+    tokbox_api_key: 45222812,
     sentry: 'https://6eec4872a99f4a379f856829b7b9ab16:061bd343010646cd8013c82bf8ea337c@app.getsentry.com/39377'
   }
 }
